565d28b6a0a1ce8e207a702a04830a4aaba9fdb3
jsososo <jsososo@outlook.com>
2019-01-20 09:46:10 +0800

    fix： 非128k音乐可能会403

--- src/assets/utils/request.js
+++ src/assets/utils/request.js
@@ -141,7 +141,7 @@ const request = {
         vkey_expire: timer().from(90, 'm').str('YYYYMMDDHHmm'),
         // 返回的url信息在播放非128k的音乐时都可能出现403，下面这个链接是从别人的qq音乐项目里找来的
         // murl: res.req_0.data.sip[1] || res.req_0.data.sip[0],
-        mul: 'http://streamoc.music.tc.qq.com/',
+        murl: 'http://streamoc.music.tc.qq.com/',
       });
     });
   },
